aba211612486c1c36a96abc325916172c44f24bb
pratap <pratap@hp-PC>
2021-07-30 15:51:00 +0530

    initial Commit

---
+++ MyRestService/src/main/java/com/skillUp/MyRestService/MyRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skillUp.MyRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestServiceApplication.class, args);
+	}
+
+}
